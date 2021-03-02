8076ad7fa260a3696d0773d64bd1559bb7b826b8
Deekshith SN <deekshithsn@gmail.com>
2021-03-02 14:29:53 +0530

    Update App.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -1,8 +1,4 @@
-import React, {useState} from 'react'
-import Header from "components/Header";
-import ExternalInfo from "components/ExternalInfo";
-import AppConfig from "App.config";
-
+import React, {useState, useEffect} from 'react'  
 
 const App = () => {
 
@@ -10,21 +6,36 @@ const App = () => {
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
   const [displayData, setDisplayData] = useState({url:''})
-   const [details, setDetails] = useState(null);
+  const [details, setDetails] = useState({});
+  let [url, setUrl] = useState('');
+
+
+
+  const getUserGeolocationDetails = () => {
+    fetch(
+        "https://geolocation-db.com/json/344ec440-6bfc-11eb-a0c0-b5dee9e67313"
+    )
+        .then(response => response.json())
+        .then(data => {
+          
+          setDetails(data)
+        
+        });
+  }
+      
+  useEffect(()=>{
+    getUserGeolocationDetails()
+  },[])
+
+
+
 
   const handleApiCall = async (e) => {
-    
-    const getUserGeolocationDetails = () => {
-        fetch(
-            "https://geolocation-db.com/json/344ec440-6bfc-11eb-a0c0-b5dee9e67313"
-        )
-            .then(response => response.json())
-            .then(data => setDetails(data));
-    };
-    
-    
-    const url = {details.IPv4}+e.target.name;
+    let url = 'http://'+details.IPv4+':32470/'+e.target.name
+    console.log(details.IPv4)
+    console.log(url)
     displayData.url=url
+    if(url){
     let options = {}
       try {
           setLoading(true)
@@ -40,6 +51,7 @@ const App = () => {
       finally {
           setLoading(false)
       }
+    }
   }
 
 
